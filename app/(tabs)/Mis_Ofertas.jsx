d7156a8fb374db34734e62c89a8a683a64035a2a
Gerardo Sanchez <gerardosanchezz14@hotmail.com>
2024-07-04 19:01:02 -0700

    Mis Ofertas view  updated

--- app/(tabs)/Mis_Ofertas.jsx
+++ app/(tabs)/Mis_Ofertas.jsx
@@ -3,8 +3,8 @@ import { SafeAreaView } from "react-native-safe-area-context";
 
 const Mis_Ofertas = () => {
   return (
-    <SafeAreaView className="px-4 my-6 bg-primary h-full">
-      <Text className="text-2xl text-white font-psemibold">Mis Ofertas</Text>
+    <SafeAreaView className="px-4 bg-primary h-full">
+      <Text className="text-2xl text-white font-psemibold my-6">Mis Ofertas</Text>
     </SafeAreaView>
   );
 };
