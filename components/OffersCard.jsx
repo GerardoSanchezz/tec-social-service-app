7eadfdd8eb7ecdfcf73e3f1016321d8ea117ca4f
Gerardo Sanchez <gerardosanchezz14@hotmail.com>
2024-07-04 18:57:59 -0700

    OffersCard front updated

--- components/OffersCard.jsx
+++ components/OffersCard.jsx
@@ -10,22 +10,30 @@ const OffersCard = ({
   cupo 
 }) => {
   return (
-    <View className="flex flex-col items-center px-4 mb-14 border border-gray-300 rounded-lg p-4 bg-gray-900">
-      <View className="w-full flex items-center">
-        <Text className="font-semibold text-lg text-white" numberOfLines={1}>
-          {nombreProyecto}
-        </Text>
-      </View>
+    <View className="flex flex-col items-center justify-center px-4 mb-14 border border-gray-300 rounded-lg p-4 bg-gray-900">
+      <Text className="font-semibold text-lg text-white text-center" numberOfLines={1}>
+        {nombreProyecto}
+      </Text>
 
       <View className="w-full flex flex-col items-center mt-3 space-y-2">
-        <Text className="text-sm text-white">Modalidad: {modalidad}</Text>
-        <Text className="text-sm text-white">
-          Carreras Preferenciales: {carrerasPreferenciales}
+        <Text className="text-sm text-white text-center">
+          <Text className="font-bold">Modalidad:</Text> {modalidad}
         </Text>
-        <Text className="text-sm text-white">Horas Máximas a Acreditar: {horasMaximas}</Text>
-        <Text className="text-sm text-white">Horario: {horario}</Text>
-        <Text className="text-sm text-white">Contacto: {contacto}</Text>
-        <Text className="text-sm text-white">Cupo: {cupo}</Text>
+        <Text className="text-sm text-white text-center">
+          <Text className="font-bold">Carreras Preferenciales:</Text> {carrerasPreferenciales}
+        </Text>
+        <Text className="text-sm text-white text-center">
+          <Text className="font-bold">Horas Máximas a Acreditar:</Text> {horasMaximas}
+        </Text>
+        <Text className="text-sm text-white text-center">
+          <Text className="font-bold">Horario:</Text> {horario}
+        </Text>
+        <Text className="text-sm text-white text-center">
+          <Text className="font-bold">Contacto:</Text> {contacto}
+        </Text>
+        <Text className="text-sm text-white text-center">
+          <Text className="font-bold">Cupo:</Text> {cupo}
+        </Text>
       </View>
     </View>
   );
